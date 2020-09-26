263c73ce89de1f4f8c9f9f12c590c1d3566cc5c5
Glitch (hello-express) <none>
2020-09-26 12:13:02 +0000

    🍉😽 Checkpoint ./commands/economy/daily.js:22751272/481 ./package.json:22751272/3770

--- commands/economy/daily.js
+++ commands/economy/daily.js
@@ -1,9 +1,24 @@
 const Discord = require('discord.js');
 const config = require('../../config.json');
-const db = require('quick.db')
+const db = require('quick.db');
+const ms = require('parse-ms')
 
 module.exports = {
   name: "daily",
   description: "get daily rewards",
-  aliases: ['']
+  aliases: ["d"],
+  usage: "daily",
+  category: "economy",
+  run:async(clients, message, args) => {
+    let prefix = await db.fetch(`prefix_${message.guild.id}`)
+    if (prefix == null) {
+      prefix = config.Default_PREFIX
+    }
+    
+    let amount = 100
+    let timeout = 8640000
+    
+    let daily = await db.fetch(`daily_${`a)
+    
+  }
 }
