f58d9ff67e434a2fbf360d27e3e4d4a67a30ac60
Glitch (hello-express) <none>
2020-09-26 11:27:47 +0000

    🎙😀 Checkpoint ./commands/moderation/warning.js:22751272/112

--- commands/moderation/warning.js
+++ commands/moderation/warning.js
@@ -10,7 +10,9 @@ module.exports = {
     const user = message.mentions.members.first() || message.author
     let warnings = db.get(`warnings_${message.guild.id}_${user.id}`)
     
+     if(warnings === null) warnings = 0;
     
+     message.channel.send(`${user} have **${warnings}** warning(s)`)
     
   }
 }
