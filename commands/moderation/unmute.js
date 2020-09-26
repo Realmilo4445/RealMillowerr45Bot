5e08f2141340d90a1e5e0f02ca73538ddc8e95db
Glitch (hello-express) <none>
2020-09-26 09:33:09 +0000

    🌁🍼 Checkpoint ./commands/moderation/mute.js:22751272/1716 ./commands/moderation/unmute.js:22751272/233

---
+++ commands/moderation/unmute.js
@@ -0,0 +1,11 @@
+module.exports = {
+  name: "unmute",
+  description: "Change the bot status",
+  usage: "unmute <@user>",
+  category: "owner",
+  run: async (client, message, args) => {
+    
+    
+    
+  }
+}
