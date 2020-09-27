5204530b920f1307c62ae7532480de279a14e077
Glitch (hello-express) <none>
2020-09-27 15:12:51 +0000

    🌞💍 Checkpoint ./commands/economy/daily.js:22751272/494

--- commands/economy/daily.js
+++ commands/economy/daily.js
@@ -7,5 +7,20 @@ module.exports = {
   name: "daily",
   aliases: ["d"],
   description: "Get daily money",
-  
+  usage: "daily",
+  run:async(client, message, args) => {
+    let prefix = await db.fetch(`prefix_${message.guild.id}`)
+    if(prefix == null) {
+      prefix = config.DEFAULT_PREFIX
+    }
+    
+    let amount = 100
+    let timeout = 86400000
+    
+    let daily = await db.fetch(`daily_${message.author.id}`)
+    
+    if(daily !== null && timeout - (Date.now() - daily) > 0) {
+      let time = ms(timeout - (Date.now()))
+    }
+  }
 }
