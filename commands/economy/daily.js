d1657c5e2edd3eaf2f6c491ec752d3e0ecf0b7a1
Glitch (hello-express) <none>
2020-09-27 15:01:04 +0000

    🎏📻 Checkpoint ./commands/economy/daily.js:22751272/204

--- commands/economy/daily.js
+++ commands/economy/daily.js
@@ -1,3 +1,11 @@
 const Discord = require('discord.js')
-const db = require('const-db')
-const ms = require
+const db = require('quick.db')
+const ms = require('parse-ms')
+const Config = require('../../config.json')
+
+module.exports = {
+  name: "daily",
+  aliases: ["d"],
+  description: "Get daily money",
+  
+}
