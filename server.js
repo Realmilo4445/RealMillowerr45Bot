fb0d4e77eec481359695251ca53c8682a61229de
Glitch (hello-express) <none>
2020-09-27 03:17:28 +0000

    🏎🌜 Checkpoint ./server.js:22751272/2219

--- server.js
+++ server.js
@@ -21,11 +21,6 @@ const client = new discord.Client({
 });
 const db = require("quick.db")
 
-const setupCMD = "!setreactionrole"
-let initialMessage = `**React to the messages below to receive the associated role. If you would like to remove the role, simply remove your reaction!**`;
-const roles = ["Announcement Ping"];
-const reactions = ["✅"];
-
 // Collections
 client.commands = new discord.Collection();
 client.aliases = new discord.Collection();
@@ -41,60 +36,7 @@ client.aliases = new discord.Collection();
 const Discord = require('discord.js');
 const bot = new Discord.Client();
  
-//If there isn't a reaction for every role, scold the user!
-if (roles.length !== reactions.length) throw "Roles list and reactions list are not the same length!";
  
-//Function to generate the role messages, based on your settings
-function generateMessages(){
-    var messages = [];
-    messages.push(initialMessage);
-    for (let role of roles) messages.push(`**React below to get the **"${role}"** role!**`); //DONT CHANGE THIS
-    return messages;
-}
- 
- 
-bot.on("message", message => {
-    if (message.member.hasPermission("ADMINISTRATOR") && message.content.toLowerCase() == setupCMD){
-        var toSend = generateMessages();
-        let mappedArray = [[toSend[0], false], ...toSend.slice(1).map( (message, idx) => [message, reactions[idx]])];
-        for (let mapObj of mappedArray){
-            message.channel.send(mapObj[0]).then( sent => {
-                if (mapObj[1]){
-                  sent.react(mapObj[1]);  
-                }
-            });
-        }
-    }
-})
- 
- 
-bot.on('raw', event => {
-    if (event.t === 'MESSAGE_REACTION_ADD' || event.t == "MESSAGE_REACTION_REMOVE"){
-       
-        let channel = bot.channels.get(event.d.channel_id);
-        let message = channel.fetchMessage(event.d.message_id).then(msg=> {
-        let user = msg.guild.members.get(event.d.user_id);
-       
-        if (msg.author.id == bot.user.id && msg.content != initialMessage){
-       
-            var re = `\\*\\*"(.+)?(?="\\*\\*)`;
-            var role = msg.content.match(re)[1];
-       
-            if (user.id != bot.user.id){
-                var roleObj = msg.guild.roles.find(r => r.name === role);
-                var memberObj = msg.guild.members.get(user.id);
-               
-                if (event.t === "MESSAGE_REACTION_ADD"){
-                    memberObj.addRole(roleObj);
-                } else {
-                    memberObj.removeRole(roleObj);
-                }
-            }
-        }
-        })
- 
-    }  
-});
 
 client.on("ready", () => { //When bot is ready
   console.log("I am Reday to Go")
@@ -198,5 +140,4 @@ client.on("guildMemberAdd", (member) => {
 })
  
 
-bot.login(process.env.TOKEN);
 client.login(token)
