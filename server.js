7b58414abef68053b2404ac123d79e514efab3dd
Glitch (hello-express) <none>
2020-10-01 10:22:25 +0000

    👬🚨 Checkpoint ./server.js:22751272/1816

--- server.js
+++ server.js
@@ -41,22 +41,9 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 
 client.on("ready", () => { //When bot is ready
-  console.log("I am Reday to Go")
+  console.log("I am Ready to Go")
   client.user.setActivity(db.get(`status`)) //It will set status :)
 })
-
- 
-
-//Url Function -Start
-function is_url(str) {
-  let regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
-  if(regexp.test(str)) {
-    return true;
-  } else {
-    return false;
-  }
-  
-}
 
 
 client.on("message", async message => {
@@ -73,13 +60,6 @@ if(message.author.bot) return;
   
   if(!message.member.hasPermission("ADMINISTRATOR")) {
    
- 
-  
-    if(is_url(message.content) === true) {
-      message.delete()
-      return message.channel.send("You can not send link here :/")
-    }
-  
     let confirm = false;
    
     var i;
@@ -144,33 +124,4 @@ client.on("guildMemberAdd", (member) => {
   client.channels.cache.get(chx).send(wembed)
 })
 
-bot.on('raw', event => {
-    if (event.t === 'MESSAGE_REACTION_ADD' || event.t == "MESSAGE_REACTION_REMOVE"){
-       
-        let channel = bot.channels.get(event.d.channel_id);
-        let message = channel.fetchMessage(event.d.message_id).then(msg=> {
-        let user = msg.guild.members.get(event.d.user_id);
-       
-        if (msg.author.id == bot.user.id && msg.content != initialMessage){
-       
-            var re = `\\*\\*"(.+)?(?="\\*\\*)`;
-            var role = msg.content.match(re)[1];
-       
-            if (user.id != bot.user.id){
-                var roleObj = msg.guild.roles.find(r => r.name === role);
-                var memberObj = msg.guild.members.get(user.id);
-               
-                if (event.t === "MESSAGE_REACTION_ADD"){
-                    memberObj.addRole(roleObj);
-                } else {
-                    memberObj.removeRole(roleObj);
-                }
-            }
-        }
-        })
- 
-    }  
-});
- 
-bot.login(process.env.TOKEN);
 client.login(token)
