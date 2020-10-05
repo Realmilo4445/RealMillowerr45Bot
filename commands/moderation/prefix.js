cc7997a14415da501e464a178ecf1f97bab09575
Glitch (hello-express) <none>
2020-10-05 14:19:49 +0000

    💁🐯 Checkpoint ./commands/moderation/prefix.js:22751272/1074

---
+++ commands/moderation/prefix.js
@@ -0,0 +1,30 @@
+const db = require("quick.db")
+const { DEFAULT_PREFIX } = require("../../config.json")
+
+module.exports = {
+  name: "prefix",
+  category: "moderation",
+  usage: "prefix <new-prefix>",
+  description: "Change the prefix",
+  run: async (client, message, args) => {
+    
+    if(!message.member.hasPermission("ADMINISTRATOR")) { return message.channel.send("You are not allowed or do not have permission to change prefix")    }
+
+   if(!args[0]) {
+      return message.channel.send("Please give the prefix that you want to set")
+    }
+    
+     if(args[1]) {
+      return message.channel.send("You can not set prefix a double argument")
+    }
+    
+     if(args.join("") === DEFAULT_PREFIX) {
+      db.delete(`prefix_${message.guild.id}`)
+     return await message.channel.send("Reseted Prefix ✅")
+    }
+    
+        db.set(`prefix_${message.guild.id}`, args[0])
+  await message.channel.send(`Seted Bot Prefix to ${args[0]}`)
+
+  }
+}
