7a9c079ee75850b8d7cf3f5a9f5e08247089c852
Glitch (hello-express) <none>
2020-10-09 03:08:29 +0000

    🚎📍 Checkpoint ./server.js:22751272/1627

--- server.js
+++ server.js
@@ -30,8 +30,42 @@ client.on("guildMemberAdd", async (member) => {
   const channel = member.guild.channels.cache.find((x) => x.name === "verify")
   
   if(!channel) {
-   return console.log(member.guild.name + "Please Create channel with name verify")
+   return console.log(member.guild.name + " Please Create channel with name verify")
   }
+  
+  const vrole = member.guild.roles.cache.find((x) => x.name === "non-verified")
+  
+    if(!vrole) {
+   return console.log(member.guild.name + " Please Create role with name 'non-verified'")
+  }
+  
+  member.roles.add(vrole)
+  
+  const verifycode = await channel.send("Please Type The Given Code For Verification",
+               new discord.MessageAttachment(captcha.PNGStream, "captcha.png")
+                                        
+  l collector = channel.createMessageCollector(m => m.author.id === member.id)
+  
+  collector.on("collect", m => {
+    if(m.content.toUpperCase() === captcha.value) {
+     m.delete()
+      verifycode.delete()
+      member.roles.remove(vrole)
+      return member.send("NOW YOU ARE VERIFIED MEMBER :)")
+    } else if(m.content.toUpperCase() !== captcha.value) {
+      member.send("You gave wrong code, so you can apply again by joining server again")
+      verifycode.delete()
+      m.delete()
+      
+      setTimeout(function() {
+              member.kick()
+      }, 3000)
+        
+    } else {
+      verifycode.delete()
+    }
+    
+  })
   
   })
 
