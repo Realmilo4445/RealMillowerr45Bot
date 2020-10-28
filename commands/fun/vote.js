75c5748a7f20e9a12b98f9a4f6411f0863758f1f
Glitch (hello-express) <none>
2020-10-28 10:25:16 +0000

    🗼🌼 Checkpoint ./commands/moderation/setwelcome.js:22751272/862 ./server.js:22751272/1205 ./commands/fun/vote.js:22751272/294

---
+++ commands/fun/vote.js
@@ -0,0 +1,13 @@
+const discord = require("discord.js");
+
+module.exports = {
+  name: "Eject",
+  category: "fun",
+  description: "ban anyone with one shot xD",
+  usage: "Eject <@user> <reason>",
+  run: (client, message, args) => {
+    
+    
+    
+  }
+}
