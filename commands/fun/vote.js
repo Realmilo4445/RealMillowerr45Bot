643f0699472f13e6cc7242e5cfaa06dc5404145b
Glitch (hello-express) <none>
2020-10-28 10:37:12 +0000

    👝🌄 Checkpoint ./commands/fun/vote.js:22751272/569 ./package.json:22751272/4973

--- commands/fun/vote.js
+++ commands/fun/vote.js
@@ -4,10 +4,10 @@ module.exports = {
   name: "Eject",
   category: "fun",
   description: "ban anyone with one shot xD",
-  usage: "Eject <@user> <reason>",
+  usage: "Eject <@user>",
   run: (client, message, args) => {
     
-    
+
     
   }
 }
