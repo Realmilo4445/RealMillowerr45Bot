b5c237e64b03dda5a7521896eb0ce2eb8f7b2874
Glitch (hello-express) <none>
2020-10-28 10:14:12 +0000

    🌒🕰 Checkpoint ./commands/moderation/setwelcome.js:22751272/862

--- commands/moderation/setwelcome.js
+++ commands/moderation/setwelcome.js
@@ -1,23 +1,8 @@
-const Discord = require("discord.js")
-const db = require("quick.db")
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const { CanvasSenpai } = require("canvas-senpai");
+const canva = new CanvasSenpai();
 
-module.exports = {
-  name: "setwelcome",
-  category: "moderation",
-  usage: "setwelcome <#channel>",
-  description: "Set the welcome channel",
-  run: (client, message, args) => {
-    
-    let channel = message.mentions.channels.first() //mentioned channel
-    
-    if(!channel) { //if channel is not mentioned
-      return message.channel.send("Please Mention the channel first")
-    }
-    
-    //Now we gonna use quick.db
-    
-    db.set(`welchannel_${message.guild.id}`, channel.id) //set id in var
-    
-    message.channel.send(`Welcome Channel is seted as ${channel}`) //send success message
-  }
-}
+client.once("ready", () => {
+  console.log("Ready!")
+})
