0666c2fdfebea8c4eb57e388c916c5c266800099
Glitch (hello-express) <none>
2020-10-28 08:13:38 +0000

    👞😂 Checkpoint ./commands/info/hello.js:22751272/2209 ./commands/info/anime.js:22751272/246 ./config.json:22751272/38

--- commands/info/hello.js
+++ commands/info/hello.js
@@ -1,10 +1,21 @@
+const { MessageEmbed } = require("discord.js")
+const { COLOR } = require("./config.json")
+
 module.exports = {
     name: "hello",
     category: "info",
     description: "Returns latency and API hello",
     usage: "hello",
     run: async (client, message, args) => {
-       message.channel.send(`**Hi!**`);
+      
+    let embed = new MessageEmbed()
+  .setTitle("Hi!")
+  .setColor("RED")
+  .setDescription(`Hello`); //Looks Cool
+      
+    if(!args.length) {
+      
+       message.channel.send(`**Hi!**`)
     }
-  
+    }
 }
