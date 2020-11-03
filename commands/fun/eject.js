775904a8add5bbdf4151155b233434eaebfeaf4a
Glitch (hello-express) <none>
2020-11-03 14:28:31 +0000

    🎠👱 Checkpoint ./commands/fun/eject.js:22751272/1029

---
+++ commands/fun/eject.js
@@ -0,0 +1,14 @@
+const { CanvasSenpai } = require("canvas-senpai");
+const canva = new CanvasSenpai();
+
+module.exports = {
+    name: "hello",
+    category: "info",
+    description: "Returns latency and API hello",
+    usage: "hello",
+    run: async (client, message, args) => {
+     
+      
+      
+    }
+}
