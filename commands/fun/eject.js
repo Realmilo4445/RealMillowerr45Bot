4c5d6297425b36192acba6e54cc4967980e2152b
Glitch (hello-express) <none>
2020-11-03 16:06:52 +0000

    🛳🏦 Checkpoint ./commands/fun/eject2.js:22751272/12 ./commands/fun/eject.js:22751272/134 ./.glitch-assets:22751272/481

--- commands/fun/eject.js
+++ commands/fun/eject.js
@@ -7,20 +7,20 @@ module.exports = {
     name: "eject",
     category: "fun",
     description: "Returns latency and API hello",
-    usage: "eject <@user>",
+    usage: "eject <@mention>",
     run: async (client, message, args) => {
  
     client.on('guildMemberAdd', async member => {
       
-    let data = await canva.welcome(member, { link: "https://wallpapercave.com/wp/wp5128415.jpg" })
+    let data = await canva.eject(member, { link: "https://i.ytimg.com/vi/D2LV7vILWTc/hqdefault.jpg" })
  
     const attachment = new Discord.MessageAttachment(
       data,
-      "welcome-image.png"
+      "eject.png"
     );
 
       
-    message.channel.send(`${member.user.username}!, Was not an Impostor`, attachment)
+    message.channel.send(`{member.user.username}!, Was not an Impostor`, attachment)
       
     })
     }
