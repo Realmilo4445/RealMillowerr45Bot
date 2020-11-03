a0c16a81798e380c8d2403570f32f3677eda1278
Glitch (hello-express) <none>
2020-11-03 15:20:22 +0000

    🍮💨 Checkpoint ./commands/fun/eject.js:22751272/1757

--- commands/fun/eject.js
+++ commands/fun/eject.js
@@ -1,14 +1,27 @@
 const { CanvasSenpai } = require("canvas-senpai");
+const Discord = require("discord.js");
+const client = new Discord.Client();
 const canva = new CanvasSenpai();
 
 module.exports = {
     name: "hello",
     category: "info",
     description: "Returns latency and API hello",
-    usage: "hello",
+    usage: "eject <@user>",
     run: async (client, message, args) => {
-     
+ 
+    client.on('guildMemberAdd', async member => {
       
+    let data = await canva.welcome(member, { link: "https://wallpapercave.com/wp/wp5128415.jpg" })
+ 
+    const attachment = new Discord.MessageAttachment(
+      data,
+      "welcome-image.png"
+    );
+
       
+    message.channel.send(`Welcome to the server, ${member.user.username}!`, attachment)
+      
+    })
     }
 }
