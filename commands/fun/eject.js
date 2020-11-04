aee3572faba5bf72b764ca04cffb278afe16f8da
Glitch (hello-express) <none>
2020-11-04 02:15:55 +0000

    🦄👧 Checkpoint ./commands/fun/eject.js:22751272/84

--- commands/fun/eject.js
+++ commands/fun/eject.js
@@ -6,7 +6,7 @@ module.exports = {
   usage: "eject <@user>",
   run: (client, message, args) => {
     
-    message.channel.send(`**${message.mentions.users.first().username}** was ejected`)
+    
     
   }
 }
