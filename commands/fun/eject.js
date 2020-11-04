84d3e11fe6bc8cdfe950c3cdffca0280ed50fdb8
Glitch (hello-express) <none>
2020-11-04 01:52:39 +0000

    🦄👶 Checkpoint ./commands/fun/eject.js:22751272/211

---
+++ commands/fun/eject.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  name: "kick",
+  category: "moderation",
+  description: "Kick anyone with one shot xD",
+  usage: "kick <@user> <reason>",
+  run: (client, message, args) => {
+    
+    
+    
+  }
+}
