49debde942e27eacdecaae71d8e0c3dd1630c51d
Glitch (hello-express) <none>
2020-11-05 13:12:53 +0000

    💪🌁 Checkpoint ./package.json:22751272/2639 ./server.js:22751272/178

--- server.js
+++ server.js
@@ -120,3 +120,12 @@ client.on("guildMemberAdd", async member => {
 });
 
 client.login(token);
+
+let count = 0;
+setInterval(
+  () =>
+    require("node-fetch")(process.env.URL).then(() =>
+      console.log(`[${++count}] here i pinged ${process.env.URL}`)
+    ),
+  300000
+);
