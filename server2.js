77032428e68728a01c28d24cec1097851332fd02
Glitch (hello-express) <none>
2020-11-07 05:33:18 +0000

    🚣👟 Checkpoint ./server2.js:22751272/1900 ./package.json:22751272/227 ./system/music.js:22751272/1765 ./commands2/drop.js:22751272/1097 ./handlers/command.js:22751272/66 ./handlers/command2.js:22751272/1628 ./commands/music/drop.js:22751272/1102

---
+++ server2.js
@@ -0,0 +1,63 @@
+const discord = require("discord.js")
+const client = new discord.Client({ disableEveryone: true, disabledEvents: ["TYPING_START"] });
+const { readdirSync } = require("fs");
+const { join } = require("path");
+const { token, DEFAULT_PREFIX } = require("./config.json")
+
+//CLIENT EVENTS
+client.on("ready", () => {
+  console.log('Ready to play song | Bot created by RealMillowerr45')
+  client.user.setActivity(">help or !help | Music and Command Bot")
+})
+
+client.on("warn", info => console.log(info));
+
+client.on("error", console.error)
+
+//DEFINIING
+client.commands = new discord.Collection()
+client.prefix = DEFAULT_PREFIX
+client.queue = new Map();
+client.vote = new Map();
+
+//LETS LOAD ALL FILES
+const cmdFiles = readdirSync(join(__dirname, "commands2")).filter(file => file.endsWith(".js"))
+for (const file of cmdFiles) {
+  const command = require(join(__dirname, "commands2", file))
+  client.commands.set(command.name, command)
+} //LOADING DONE
+
+
+//WHEN SOMEONE MESSAGE
+client.on("message", message => {
+   if (message.author.bot) return;
+  if (!message.guild) return;
+  
+  if(message.content.startsWith(DEFAULT_PREFIX)) { //IF MESSSAGE STARTS WITH MINE BOT PREFIX
+    
+    const args = message.content.slice(DEFAULT_PREFIX.length).trim().split(/ +/) //removing prefix from args
+    const command = args.shift().toLowerCase();
+    
+    if(!client.commands.has(command)) {
+      return;
+    } 
+    
+  try  { //TRY TO GET COMMAND AND EXECUTE
+      client.commands.get(command).execute(client, message, args)
+    //COMMAND LOGS
+    console.log(`${message.guild.name}: ${message.author.tag} Used ${client.commands.get(command).name} in #${message.channel.name}`)
+    } catch (err) { //IF IT CATCH ERROR
+      console.log(err)
+      message.reply("I am getting error on using this command")
+    }
+    
+  }
+  
+  
+});
+
+
+
+
+//DONT DO ANYTHING WITH THIS TOKEN lol
+client.login(token)
