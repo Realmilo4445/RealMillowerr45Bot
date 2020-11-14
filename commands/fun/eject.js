6aab0709a9c38990f977819913e16389c8ac3309
Glitch (hello-express) <none>
2020-11-14 15:12:39 +0000

    🦂🐉 Checkpoint ./commands/fun/eject.js:22751272/3304

--- commands/fun/eject.js
+++ commands/fun/eject.js
@@ -1,15 +1,29 @@
 const fs = require('fs')
-const discord = require('discord.js')
+const { MessageEmbed } = require('discord.js')
 const db = require('quick.db');
 
 module.exports = {
   name: "eject",
   category: "fun",
-  description: "eject",
+  description: "eject <@metion>",
   run: async(client, message, args) => {
+    
+    let Embed = new MessageEmbed()
     
     const url = "https://lh3.googleusercontent.com/DjukZ3eSbRkBpc_1KX-Q7H0zKQqi1QkaZJtgD0TZnogoYC4QKfIlCc53DT4LH4f228AudTza=w1280"
     
+    const user = message.mentions.members.first()
+    
+     if(!user) {
+      return message.channel.send("Please Mention the person to who you want to warn - warn @mention <reaosn>")
+    }
+    
+    Embed.setTitle(`${message.mentions.users.first().username} Was not an impostor`)
+    
+      if(message.mentions.users.first().username) {
+      return message.channel.send(Embed)
+    }
+
     
   }
 }
