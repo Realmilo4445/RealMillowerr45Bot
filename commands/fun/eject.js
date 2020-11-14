b6d4a691d5cb374407cc34beec28ae2d0fcc5f5f
Glitch (hello-express) <none>
2020-11-14 13:05:20 +0000

    🏙🏊🏼 Checkpoint ./commands/fun/eject.js:22751272/276

--- commands/fun/eject.js
+++ commands/fun/eject.js
@@ -1,10 +1,18 @@
+const fs = require('fs')
+const discord = require('discord.js')
 
 module.exports = {
   name: "eject",
   category: "fun",
   description: "eject",
   usage: "eject <@user>",
-  run: (client, message, args) => {
+  run: async(client, message, args) => {
+    
+    
+    
+    const url = "https://lh3.googleusercontent.com/DjukZ3eSbRkBpc_1KX-Q7H0zKQqi1QkaZJtgD0TZnogoYC4QKfIlCc53DT4LH4f228AudTza=w1280"
+    
+    msg = message.content.toLowerCase();
     
     
     
