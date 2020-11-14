f92e41d24129b5da193d3c0d3187dc1112b9cf7f
Glitch (hello-express) <none>
2020-11-14 15:59:07 +0000

    👁🐉 Checkpoint ./package.json:22751272/3382 ./commands/fun/amongusCategory.js:22751272/78 ./commands/fun/Among-Us-Category.js:22751272/35 ./commands/fun/among-us-category.js:22751272/662

---
+++ commands/fun/among-us-category.js
@@ -0,0 +1,20 @@
+const Commando = require('discord.js-commando')
+
+module.exports = class AmongUsCategoryCommand extends Commando.Command {
+  constructor(client) {
+    super(client, {
+      name: 'aucat',
+      group: 'fun',
+      memberName: 'aucat',
+      userPermissions : [
+        'ADMINISTRATOR'
+      ],
+      description: "",
+      run: async(client messah)
+    })
+  }
+  
+    
+    const
+  }
+}
