ebd9e7af0f7977321983f82edfe7acd3367e525e
Glitch (hello-express) <none>
2020-11-15 01:06:26 +0000

    💓🍨 Checkpoint ./commands/info/eject.js:22751272/780

---
+++ commands/info/eject.js
@@ -0,0 +1,24 @@
+const { COLOR } =  require('../../config.json')
+const { MessageEmbed } = require('discord.js')
+
+module.exports = {
+  name: "eject",
+  description: "eject all",
+  category: "info",
+  usage: "eject <@mentions>",
+  run: async(client, message, args) => {
+    let Embed = new MessageEmbed()
+    .setColor(COLOR)
+    Embed.setTitle(` **${message.mentions.users.first().username}** Was not an impostor`)
+    message.channel.send(Embed)
+    
+  }
+ 
+  
+   let Embed = new MessageEmbed()
+    .setColor(COLOR)
+    Embed.setTitle(` **${message.mentions.users.first().username}** Was not an impostor`)
+    message.channel.send(Embed)
+
+}
+}
