b2d8d9083835c5e3c30b831291374a5b828a8530
Glitch (hello-express) <none>
2021-01-07 13:43:14 +0000

    🌥👩‍❤️‍💋‍👩 Checkpoint ./commands/info/eject.js:22751272/974

--- commands/info/eject.js
+++ commands/info/eject.js
@@ -1,24 +1,22 @@
 const { MessageEmbed } = require("discord.js");
 module.exports = {
-  name: "8ball",
+  name: "eject",
   description: "There is a big chance I insult you!",
   category: "fun",
   run: async (bot, message, args) => {
     let Embed = new MessageEmbed()
-    let question = message.content.slice(bot.prefix.length + 6);
-    if (!question)
+    let eject = message.content.slice();
       return message.channel.send(`You did not specify your question!`);
     else {
       let responses = [
-        Embed.SetTitle(` **${message.mentions.users.first().username}** Was an impostor`),
-        Embed.SetTitle(` **${message.mentions.users.first().username}** Was an impostor`),
-        Embed.SetTitle(` **${message.mentions.users.first().username}** Was ejected`)
+        (` **${message.mentions.users.first().username}** Was an impostor`),
+        (` **${message.mentions.users.first().username}** Was an impostor`),
+        (` **${message.mentions.users.first().username}** Was ejected`)
       ];
       let response =
-        responses[Math.floor(Math.random() * responses.length - 1)];
+        responses[Math.floor(Math.random())];
       let Embed = new MessageEmbed()
-        .setTitle(`8Ball!`)
-        .setDescription(`Your question: ${question}\nMy reply: ${response}`)
+        .setDescription(`${response}`)
         .setColor(`RED`);
       message.channel.send(Embed);
     }
