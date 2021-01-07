277f3b5f1aea83558e9773a6fbccf5040b93f536
Glitch (hello-express) <none>
2021-01-07 13:32:07 +0000

    🌲☔️ Checkpoint ./commands/info/eject.js:22751272/3411

--- commands/info/eject.js
+++ commands/info/eject.js
@@ -1,25 +1,26 @@
-const { COLOR } =  require('../../config.json')
-const { MessageEmbed } = require('discord.js')
-
+const { MessageEmbed } = require("discord.js");
 module.exports = {
-  name: "eject",
-  description: "eject all",
-  category: "info",
-  usage: "eject <@mentions>",
-  run: async(client, message, args) => {
+  name: "8ball",
+  description: "There is a big chance I insult you!",
+  category: "fun",
+  run: async (bot, message, args) => {
     let Embed = new MessageEmbed()
-    .setColor(COLOR)
-    
-    
-    const user = message.mentions.member.first()
-     Embed.setTitle(`**${message.mentions.users.first().username}** Was not an impostor`)
-       
-       if (!user) {
-         Embed.setTitle(`**${message.mentions.users.first().username}** Was not an impostor`)
-       }
-         
-        else if (!user) {
-           Embed.setTitle(`**${message.mentions.users.first().username}** Was an impostor`)  
-     }
-  }
-}
+    let question = message.content.slice(bot.prefix.length + 6);
+    if (!question)
+      return message.channel.send(`You did not specify your question!`);
+    else {
+      let responses = [
+        Embed.SetTitle(` **${message.mentions.users.first().username}** Was an impostor`),
+        Embed.SetTitle(` **${message.mentions.users.first().username}** Was an impostor`),
+        Embed.SetTitle(` **${message.mentions.users.first().username}** Was ejected`)
+      ];
+      let response =
+        responses[Math.floor(Math.random() * responses.length - 1)];
+      let Embed = new MessageEmbed()
+        .setTitle(`8Ball!`)
+        .setDescription(`Your question: ${question}\nMy reply: ${response}`)
+        .setColor(`RED`);
+      message.channel.send(Embed);
+    }
+  },
+};
