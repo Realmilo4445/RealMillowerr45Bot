c675e1c0f80f846c02c2e034cb1e433edffd7d05
Glitch (hello-express) <none>
2021-01-08 04:02:30 +0000

    🌉🦄 Checkpoint ./commands/fun/8ball.js:22751272/2633 ./commands/info/ship.js:22751272/1336

--- commands/fun/8ball.js
+++ commands/fun/8ball.js
@@ -1,20 +1,56 @@
-const Discord = require('discord.js')
-const { COLOR } = require('../../config.json')
+const { Message, MessageEmbed } = require("discord.js");
+
+const answers = [
+  "It is certain.",
+  "It is decidely so.",
+  "Without a doubt.",
+  "Yes, definitely.",
+  "You may rely on it.",
+  "As I see it, yes.",
+  "Most likely",
+  "Outlook good.",
+  "Reply hazy, try again.",
+  "Ask again later.",
+  "Never.",
+  "Cannot predict now.",
+  "Concentrate, and ask again.",
+  "Don't count on it.",
+  "No.",
+  "Maybe.",
+  "Outlook not so good.",
+  "Yes.",
+];
 
 module.exports = {
   name: "8ball",
-  aliases: [""],
+  description: "This command gives a response from the 8ball",
   category: "fun",
-  usage: "8ball <question>",
+
+  //IntelliSense
+  /**
+   * @param {Message} message
+   * @param {String[]} args
+   */
+
   run: async (client, message, args) => {
-    let answer = ["Yes", "No", "Maybe","Undefined"]
-    if(!args.slice(0).join(" ")) return message.channel.send("Where is the question?")
-    
-    let ranswer = Math.floor((Math.random() + answer.length))
-    
-    let Embed = new Discord.MessageEmbed()
-    .setColor(COLOR)
-    .setTitle("Your question", args.slice(1).join(" "), "Answer", `${answer[ranswer]}`)
-    message.channel.send(Embed)
-  }
-}
+    //Create an instance of the UtilityEmbeds class;
+
+    const question = message.content.slice(7);
+
+    if (!question) {
+      return message.channel.send(
+          "You did not provide your question!",
+          `Triggered by ${message.author.tag}`
+      )
+    }
+
+    let chosenAnswer = answers[Math.floor(Math.random() * answers.length)];
+
+    const embed = new MessageEmbed();
+    embed.setTitle("🎱 8ball 🎱");
+    embed.setDescription(chosenAnswer);
+    embed.setFooter(`Requested by ${message.author.tag}`);
+
+    message.channel.send(embed);
+  },
+};
