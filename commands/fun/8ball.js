91971546db67967ca53d5788def2c1876f7c8358
Glitch (hello-express) <none>
2021-01-08 03:51:13 +0000

    ⛺️🍭 Checkpoint ./commands/fun/8ball.js:22751272/702

--- commands/fun/8ball.js
+++ commands/fun/8ball.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js')
+const { COLOR } = require('../../config.json')
 
 module.exports = {
   name: "8ball",
@@ -6,7 +7,14 @@ module.exports = {
   category: "fun",
   usage: "8ball <question>",
   run: async (client, message, args) => {
-    let answer = ["Yes", "No", "Maybe"]
-    if(!args.slice(0).join())
+    let answer = ["Yes", "No", "Maybe","Undefined"]
+    if(!args.slice(0).join(" ")) return message.channel.send("Where is the question?")
+    
+    let ranswer = Math.floor((Math.random() + answer.length))
+    
+    let Embed = new Discord.MessageEmbed()
+    .setColor(COLOR)
+    .setTitle("Your question", args.slice(1).join(" "), "Answer", `${answer[ranswer]}`)
+    message.channel.send(Embed)
   }
 }
