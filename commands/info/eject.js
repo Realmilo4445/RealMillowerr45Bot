420fc0a1ee0fc1370d42cc24dd98374ed138ee2b
Glitch (hello-express) <none>
2021-01-08 04:13:53 +0000

    🏄🗻 Checkpoint ./commands/info/eject.js:22751272/2839

--- commands/info/eject.js
+++ commands/info/eject.js
@@ -1,8 +1,39 @@
-const { MessageEmbed } = require("discord.js");
+const { Message, MessageEmbed } = require("discord.js");
+
 module.exports = {
   name: "eject",
-  description: "There is a big chance I insult you!",
+  description: "eject",
   category: "fun",
+
+  //IntelliSense
+  /**
+   * @param {Message} message
+   * @param {String[]} args
+   */
+
   run: async (client, message, args) => {
+    
+const answers = [
+  (`**${message.mentions.users.first().username}** Was an impostor`),
+  (`**${message.mentions.users.first().username}** Was not an impostor`),
+  (`**${message.mentions.users.first().username}** Was ejected`),
+];
+    //Create an instance of the UtilityEmbeds class;
+
+    const question = message.content.slice(7);
+
+    if (!question) {
+      return message.channel.send(
+          "please select to want eject",
+          `Triggered by ${message.author.tag}`
+      )
+    }
+
+    let chosenAnswer = answers[Math.floor(Math.random() * answers.length)];
+
+    const embed = new MessageEmbed();
+    embed.setDescription(chosenAnswer);
+
+    message.channel.send(embed);
   },
 };
