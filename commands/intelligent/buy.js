b640d48ee4d083635999002cc883b4f4e6653efb
Glitch (hello-express) <none>
2021-01-09 10:01:22 +0000

    🙅💸 Checkpoint ./commands/moderation/stats.js:25062700/137 ./commands/intelligent/buy.js:25062700/1303

--- commands/intelligent/buy.js
+++ commands/intelligent/buy.js
@@ -24,5 +24,11 @@ module.exports = {
             db.push(message.author.id, "Watch");
             message.channel.send('Successfully bought one car')
         }
+        if(purchase === 'watch' || 'Watch'){
+            if(amount < 250) return message.channel.send('You do not have enough money to buy this item. Please try another one');
+            db.subtract(`money_${message.guild.id}_${message.author.id}`, 250);
+            db.push(message.author.id, "Watch");
+            message.channel.send('Successfully bought one car')
+        }
     }
 }
