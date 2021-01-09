0195b1128f608e43f7f654d326d903b4257f2bdb
Glitch (hello-express) <none>
2021-01-09 03:58:56 +0000

    💝🚐 Checkpoint ./commands/economy/detective.js:25062700/688

--- commands/economy/detective.js
+++ commands/economy/detective.js
@@ -1,7 +1,10 @@
 const { MessageEmbed } = require('discord.js')
 const { COLOR } = require('../../config.json')
 const question = [
-  [``]
+  c["One day in the house there was a corpse impaled in Mr. Michael's neck and the detectives thought there were three people, one of the three people was the murderer."]
+  ["A: Im cleaning in Mr. Michaels room."]
+  ["B: Im preparing a drinking bottle for Mr. Michael."]
+  ["C: Im taking the task from Mr. Michael again. who is lying?"]
 ]
 
 module.exports = {
