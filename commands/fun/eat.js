bd7fe20ad9fd317d6170a98e0a6e438133a56806
Glitch (hello-express) <none>
2021-01-11 09:50:38 +0000

    🍳🎿 Checkpoint ./commands/fun/eat.js:25062700/6

--- commands/fun/eat.js
+++ commands/fun/eat.js
@@ -10,7 +10,7 @@ module.exports = {
      let Embed = new MessageEmbed()
      const content = args.join(" ")
      Embed.setColor(COLOR)
-     Embed.setDescription(`You eat **${content}**`)
+     Embed.setDescription(`You ate **${content}**`)
      Embed.setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic : true }))
      message.channel.send(Embed)
   }
