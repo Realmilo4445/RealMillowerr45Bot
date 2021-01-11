6e0b50d7b23c84ce1f351b00c22b498e17052564
Glitch (hello-express) <none>
2021-01-11 12:25:00 +0000

    ✍️👶 Checkpoint ./commands/economy/give.js:25062700/641

--- commands/economy/give.js
+++ commands/economy/give.js
@@ -12,7 +12,19 @@ module.exports = {
     let Embed = new Discord.MessageEmbed()
         const profiles = new db.table('profiles')
         
-        const mntn = 
+        const mntn = message.mentions.members.first()
+        
+        if(!mntn) return message.channel.send(`**You need to mention a member to give money to**`)
+      
+        const member = profiles.get(`profiles_${mntn.id}`)
+        
+        const msgmember = profiles.get(`profiles_${message.member.id}`)
+        
+        if(!member) return message.channel.send(`**This member doesn't have a profile**`)
+        
+        if(!msgmember) return message.channel.send(`**You don't have a profile**`)
+      
+       
         Embed.setDescription(`Added **$`)
         Embed.setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic : true }))
         message.channel.send(Embed)
