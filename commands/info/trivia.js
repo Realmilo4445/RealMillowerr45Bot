0c9fab6b1b8b93c05a2ffed1e67e1fc1a61c392f
Glitch (hello-express) <none>
2021-01-12 06:59:14 +0000

    🌸🐦 Checkpoint ./package.json:25062700/7802 ./commands/info/trivia.js:25062700/621

---
+++ commands/info/trivia.js
@@ -0,0 +1,31 @@
+
+
+[
+  {
+    "title": "Question text?",
+    "kind": "single|multiple|text",
+    "code_block": "",
+    "resolution": [
+      3
+    ],
+    "difficulty": "5",
+    "answers": [
+      {
+        "content": "-h",
+        "explanation": "ls -lh shows the size values in **h**uman readable format."
+      },
+      {
+        "content": "-l",
+        "explanation": "ls -l is the list view."
+      },
+      {
+        "content": "-r",
+        "explanation": "ls -r reversely orders the output (DESC)."
+      },
+      {
+        "content": "-a",
+        "explanation": "ls -a shows hidden files starting with a dot."
+      }
+    ]
+  }
+]
