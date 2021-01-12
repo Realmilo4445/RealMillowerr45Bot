33f562708220caf2ca454ad76eba41d92d9cf360
Glitch (hello-express) <none>
2021-01-12 04:54:20 +0000

    🏚🎙 Checkpoint ./commands/owner/hack.js:25062700/295

---
+++ commands/owner/hack.js
@@ -0,0 +1,11 @@
+const ms = require('ms')
+
+module.exports = {
+  name: "hack",
+  category: "owner",
+  description: "dont use it!",
+  run: async (client, message, args) => {
+    if(!args[0]) return message.channel.send(`Tell me who to hack`)
+    cosnt
+  }
+}
