c3fae2b1fb523cfb79635c7576ea282fbffab8d8
Glitch (hello-express) <none>
2021-01-14 09:41:57 +0000

    🐦👨‍❤️‍👨 Checkpoint ./commands/fun/avocados.js:25062700/1470

---
+++ commands/fun/avocados.js
@@ -0,0 +1,49 @@
+const { Message, MessageEmbed } = require("discord.js");
+
+const answers = [
+  "It is certain.",
+  "It is decidely so.",
+  "Without a doubt.",
+  "Yes, definitely.",
+  "You may rely on it.",
+  "As I see it, yes.",
+  "Most likely",
+  "Outlook good.",
+  "Reply hazy, try again.",
+  "Ask again later.",
+  "Never.",
+  "Cannot predict now.",
+  "Concentrate, and ask again.",
+  "Don't count on it.",
+  "No.",
+  "Maybe.",
+  "Outlook not so good.",
+  "Yes.",
+];
+
+module.exports = {
+  name: "8ball",
+  description: "This command gives a response from the 8ball",
+  category: "fun",
+
+  //IntelliSense
+  /**
+   * @param {Message} message
+   * @param {String[]} args
+   */
+
+  run: async (client, message, args) => {
+    //Create an instance of the UtilityEmbeds class;
+
+  
+
+    let chosenAnswer = answers[Math.floor(Math.random() * answers.length)];
+
+    const embed = new MessageEmbed();
+    embed.setTitle("🎱 8ball 🎱");
+    embed.setDescription(chosenAnswer);
+    embed.setFooter(`Requested by ${message.author.tag}`);
+
+    message.channel.send(embed);
+  },
+};
