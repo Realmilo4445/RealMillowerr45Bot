34573b988d230e4073e11cb4923ebad02c5dcd59
Glitch (hello-express) <none>
2021-01-15 05:44:17 +0000

    🍿🏑 Checkpoint ./commands/fun/avocados.js:25062700/134

--- commands/fun/avocados.js
+++ commands/fun/avocados.js
@@ -16,7 +16,6 @@ module.exports = {
   run: async (client, message, args) => {
     const Embed = new MessageEmbed();
     let user = message.author;
-    let Mexico = `**Mexico**`
 const answers = [
   `**Mexico**`,
   `**Indian**`,
@@ -28,12 +27,12 @@ const answers = [
     let msg = await Embed.setDescription(`Avocados from **${chosenAnswer}**!`)
     message.channel.send(Embed)
     let time = '5s'
-    const embed = new MessageEmbed();
     setTimeout(function(){
+      const embed = new MessageEmbed();
       db.add(`money_${message.guild.id}_${user.id}`, amount)
       embed.setDescription(`Congrats you got **${amount}** Moneys from **${chosenAnswer}**!`)
       embed.setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic : true }));
-      message.channel.send(embed);
+      message.edit(embed);
     }, ms (time))
     //Create an instance of the UtilityEmbeds class
   },
