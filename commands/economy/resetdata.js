56a0dfc009e56682e2289e84fecd5c84530764a0
Glitch (hello-express) <none>
2021-01-16 08:49:14 +0000

    🐈🙇 Checkpoint ./commands/economy/resetdata.js:25062700/1346

--- commands/economy/resetdata.js
+++ commands/economy/resetdata.js
@@ -4,9 +4,9 @@ const Discord = require('discord.js');
 const { COLOR } = require('../.././config.json')
 
 module.exports = {
-    name: "bal",
+    name: "resetdata",
   category: "economy",
-    description: "Your balance",
+    description: "Reset your balance",
 
     async run (client, message, args) {
 let Embed = new Discord.MessageEmbed()
@@ -14,8 +14,21 @@ let Embed = new Discord.MessageEmbed()
         Embed.setColor(COLOR)
         Embed.setDescription(`**Reset Data...**`)
         Embed.setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic : true }))
-        let msg = message.channel.send(Embed)
-        let time = '5s'
-        
+        let msg = await message.channel.send(Embed)
+        let time = '3s'
+        setTimeout(function(){
+          Embed.setColor(COLOR)
+        Embed.setDescription(`**Reset Data..**`)
+        Embed.setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic : true }))
+        }, ms (time))
+        let times = '6s'
+        setTimeout(function(){
+          db.subtract(`money_${message.guild.id}_${message.author.id}`, 999999999);
+          let sebed = new Discord.MessageEmbed()
+          sebed.setColor(COLOR)
+          sebed.setDescription(`**Success Reseting Your Data**`)
+          sebed.setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic : true }))
+          msg.edit(sebed)
+        }, ms (times))
     }
 }
