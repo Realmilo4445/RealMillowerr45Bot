bc6ff71f7b7b040deb677d973a3098f16d7828d0
Glitch (hello-express) <none>
2021-01-16 04:02:57 +0000

    🎙🏎 Checkpoint ./commands/owner/owner.js:25062700/624

--- commands/owner/owner.js
+++ commands/owner/owner.js
@@ -12,9 +12,10 @@ module.exports = {
     const dnt = '[Donate](https://saweria.co/Millowerr45)'
     const ha = '[Roblox Profile](https://web.roblox.com/users/1870151782/profile)'
     Embed.setColor(COLOR)
-    Embed.setTitle(`Support the Owner RealMillowerr45!`)
-    Embed.setAuthor('Support Owner To Get Premium Role! + New Command! + Give Roblox Soon!')
-    Embed.setDescription(`Support on **[Youtube](https://www.youtube.com/channel/UC3J84aa7K_uCZwBwg6j2pnA)**, [Twitter](https://twitter.com/millowerr45), [Donate](https://saweria.co/Millowerr45), [Roblox Profile](https://web.roblox.com/users/1870151782/profile)`)
+    Embed.setAuthor(`Support the Owner RealMillowerr45!`)
+    Embed.setImage(message.author.avatarURL)
+    Embed.setTitle('Support Owner To Get Premium Role! + New Command! + Give Roblox Soon!')
+    Embed.setDescription(`**Support on** **[Youtube](https://www.youtube.com/channel/UC3J84aa7K_uCZwBwg6j2pnA)**, **[Twitter](https://twitter.com/millowerr45)**, **[Donate](https://saweria.co/Millowerr45)**, **[Roblox Profile](https://web.roblox.com/users/1870151782/profile)** `)
     message.channel.send(Embed)
   }
 }
