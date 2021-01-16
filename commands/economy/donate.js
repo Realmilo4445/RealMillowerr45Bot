54bc019b1ac282397c51e5a392c52aa311eb2c1c
Glitch (hello-express) <none>
2021-01-16 04:49:50 +0000

    🐿🏹 Checkpoint ./commands/economy/donate.js:25062700/852

--- commands/economy/donate.js
+++ commands/economy/donate.js
@@ -24,28 +24,17 @@ const answers = [
 ];
     let chosenAnswer = answers[Math.floor(Math.random() * answers.length)];
       const Embed = new MessageEmbed();
-    const smbed = new MessageEmbed();
+      const smbed = new MessageEmbed();
       Embed.setDescription(`Send donate to **${chosenAnswer}**!`)
-    let users = message.author;
-        let timeout = 8640;
-
-        let daily = await db.fetch(`daily_${message.guild.id}_${user.id}`);
-
-        if(daily !== null && timeout - (Date.now() - daily) > 0){
-            let time = ms(timeout - (Date.now() - daily));
-          Embed.setAuthor(`(❌) Hey! Slowdown wait ${time.seconds} seconds for donate`)
-            return message.channel.send(smbed)
-        } else {
+      let msg = await message.channel.send(Embed)
       let time = "5s"
       setTimeout(function(){
       const embed = new MessageEmbed()
     embed.setDescription(`Congrats you got donate **${amount}** Moneys from **${chosenAnswer}**!`);
     db.add(`money_${message.guild.id}_${user.id}`, amount)
     embed.setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic : true }));
-    message.channel.send(embed)
+    msg.edit(embed)
     }, ms (time))
-    message.channel.send(Embed)
     //Create an instance of the UtilityEmbeds class
-      }
   },
 };
