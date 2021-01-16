55847f04c62210bd8f83b1d32f1727720faf0565
Glitch (hello-express) <none>
2021-01-16 03:51:50 +0000

    📉🌵 Checkpoint ./commands/owner/owner.js:25062700/876

--- commands/owner/owner.js
+++ commands/owner/owner.js
@@ -1,17 +1,20 @@
 const Discord = require('discord.js')
-const { COLOR } = require('../../../config.json')
+const { COLOR } = require('../.././config.json')
 
 module.exports = {
-  name: "link",
+  name: "owner",
   category: "moderation",
   description: "Send link to invite my bot",
   run : async (client, message, args) => {
     let Embed = new Discord.MessageEmbed()
-    const here = '[invite](https://discord.com/api/oauth2/authorize?client_id=773761326858240020&permissions=8&scope=bot)'
+    const here = '[Youtube](https://www.youtube.com/channel/UC3J84aa7K_uCZwBwg6j2pnA)'
+    const lol = '[Twitter](https://twitter.com/millowerr45)'
+    const dnt = '[Donate](https://saweria.co/Millowerr45)'
+    const ha = '[Roblox Profile](https://web.roblox.com/users/1870151782/profile)'
     Embed.setColor(COLOR)
-    Embed.setTitle(`Support the Owner`)
-    Embed.setDescription('')
-    Embed
+    Embed.setTitle(`Support the Owner RealMillowerr45!`)
+    Embed.setAuthor('Support Owner To Get Premium Role! + New Command! + Give Roblox Soon!')
+    Embed.setDescription(`Support on **[Youtube](https://www.youtube.com/channel/UC3J84aa7K_uCZwBwg6j2pnA)**, [Twitter](https://twitter.com/millowerr45), [Donate](https://saweria.co/Millowerr45), [Roblox Profile](https://web.roblox.com/users/1870151782/profile)`)
     message.channel.send(Embed)
   }
 }
