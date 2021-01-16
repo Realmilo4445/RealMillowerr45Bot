09810557a4983ceaa69d8fdc3536a0a589e96c5f
Glitch (hello-express) <none>
2021-01-16 03:04:28 +0000

    ⛷🐵 Checkpoint ./commands/fun/avocados.js:25062700/28 ./commands/economy/donate.js:25062700/1868

--- commands/economy/donate.js
+++ commands/economy/donate.js
@@ -3,9 +3,9 @@ const db = require('quick.db');
 const ms = require('ms')
 let amount = Math.floor(Math.random() * 80) + 10
 module.exports = {
-  name: "avocados",
-  description: "This command gives a response from the avocados",
-  category: "fun",
+  name: "donate",
+  description: "This command gives a response from the donate",
+  category: "economy",
 
   //IntelliSense
   /**
@@ -25,11 +25,14 @@ const answers = [
     let chosenAnswer = answers[Math.floor(Math.random() * answers.length)];
       const Embed = new MessageEmbed();
       db.add(`money_${message.guild.id}_${user.id}`, amount)
-      Embed.setDescription(`Avocados from **${chosenAnswer}**!`)
-    const embed = new MessageEmbed()
-    embed.setDescription(`Congrats you got **${amount}** Moneys from **${chosenAnswer}**!`);
+      Embed.setDescription(`Send donate to **${chosenAnswer}**!`)
+    let time = "10s"
+    setTimeout(function(){
+      const embed = new MessageEmbed()
+    embed.setDescription(`Congrats you got donate **${amount}** Moneys from **${chosenAnswer}**!`);
     embed.setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic : true }));
-    message.channel.edit(embed);
+    message.channel.send(embed)
+    },ms (time))
     message.channel.send(Embed)
     //Create an instance of the UtilityEmbeds class
   },
