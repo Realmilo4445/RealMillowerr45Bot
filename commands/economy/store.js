c02633fee9bb95680b20a46569d20299122e55c7
Glitch (hello-express) <none>
2021-01-20 07:00:28 +0000

    💑🏐 Checkpoint ./commands/economy/store.js:25062700/239

--- commands/economy/store.js
+++ commands/economy/store.js
@@ -16,14 +16,15 @@ module.exports = {
         
         await msg.react(nextPageEmoji)
         await msg.react(nextPreviousEmoji)
-        await msg.awaitReactions((reaction, user) => user.id == user.id && (reaction.emoji.name == nextPreviousEmoji||reaction.emoji.name == nextPageEmoji))
+        await msg.awaitReactions((reaction, user) => user.id == user.id && (reaction.emoji.name == nextPreviousEmoji||reaction.emoji.name == nextPageEmoji),{max: 1, max:10000})
         .then(async collected => {
           let s = new Discord.MessageEmbed()
         .setTitle('Store')
         .setDescription(`**(💍)Ring** - **500** Moneys \n **(⏰)Watch** - **250** Moneys \n **(🎁)Lootbox** - **300** Moneys`)
         .setTimestamp();
-          if(collected.first().emoji.name == nextPageEmoji){ return message.channel.send(s) }
-          if(collected.first().emoji.name == nextPreviousEmoji){ return message.channel.send(embed) }
-        })
+          if(collected.first().emoji.name == nextPageEmoji){ return msg.edit(s) }
+          if(collected.first().emoji.name == nextPreviousEmoji){ return msg.edit(embed) }
+          else return message.channel.send('error')
+        }).catch(async () => { return message.channel.send('error')})
       }
     }
