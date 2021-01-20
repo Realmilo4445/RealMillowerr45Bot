739132a26aaad64ffbda17bad2294ef0744a1753
Glitch (hello-express) <none>
2021-01-20 10:28:40 +0000

    🙏👰 Checkpoint ./commands/economy/store.js:25062700/1598

--- commands/economy/store.js
+++ commands/economy/store.js
@@ -7,11 +7,41 @@ module.exports = {
 
     async run (client, message, args) {
 
-        const embed = new Discord.MessageEmbed()
+        const one = new Discord.MessageEmbed()
         .setTitle('Store')
         .setDescription(`**(🚗)Car** - **500** Moneys \n **(⏰)Watch** - **250** Moneys`)
+        .setFooter(`Page 1/4`)
         .setTimestamp();
 
-        let msg = message.channel.send(embed);
+        let msg = message.channel.send(one);
+      
+      const two = new Discord.MessageEmbed()
+        .setTitle('Store')
+        .setDescription(`**(🚗)Car** - **500** Moneys \n **(⏰)Watch** - **250** Moneys`)
+        .setFooter(`Page 2/4`)
+        .setTimestamp();
+
+      
+      msg.react('⏪').then(() => message.react('◀')).then(() => message.react('▶')).then(() => message.react('⏩'));
+
+const filter = (reaction, user) => {
+	return ['⏪', '◀', '▶', '⏩'].includes(reaction.emoji.name) && user.id === message.author.id;
+};
+
+msg.awaitReactions(filter, { max: 4, time: 60000, errors: ['time'] })
+	.then(collected => {
+		const reaction = collected.first();
+
+		if (reaction.emoji.name === '▶') {
+			msg.edit(two)
+		} else if (reaction.emoji.name === '◀') {
+			msg.edit(one)
+    }else if (reaction.emoji.name === '⏪') {
+			msg.edit(one)
+    }
+	})
+	.catch(collected => {
+		message.reply('you reacted with neither a thumbs up, nor a thumbs down.');
+	});
     }
 }
