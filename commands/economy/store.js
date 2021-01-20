674a08e141eacf871d55da3e5093b631a44dce47
Glitch (hello-express) <none>
2021-01-20 10:04:24 +0000

    🚗🐵 Checkpoint ./commands/economy/store.js:25062700/2845

--- commands/economy/store.js
+++ commands/economy/store.js
@@ -1,44 +1,6 @@
 
 const Discord = require('discord.js');
-const nextPageEmoji = "➡"
-const nextPreviousEmoji = "⬅"
+const nextPageEmoji =eeee
+const nextPreviousEmoji = "⬅"e
 module.exports = {
-    name: "store",
-  category: "economy",
-    description: "View the store",
-
-    run: async (client, message, args) => {
-        const embed = new Discord.MessageEmbed()
-        .setTitle('Store')
-        .setDescription(`**(🚗)Car** - **500** Moneys \n **(⏰)Watch** - **250** Moneys \n **(🎁)Lootbox** - **300** Moneys`)
-        .setTimestamp()
-        .setFooter(`Pa`);
-      let msg = await message.channel.send(embed)
-        
-          let s = new Discord.MessageEmbed()
-        .setTitle('Store')
-        .setDescription(`**(💍)Ring** - **280** Moneys \n **(🍔)Hamburger** - **50** Moneys \n **(🌭)Hotdog** - **10** Moneys`)
-        .setTimestamp()
-        .setFooter(`Page 2/2`);
-msg.react('➡').then(() => msg.react('⬅'));
-
-const filter = (reaction, user) => {
-	return ['➡', '⬅'].includes(reaction.emoji.name) && user.id === message.author.id;
-};
-
-msg.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
-	.then(collected => {
-		const reaction = collected.first();
-
-		if (reaction.emoji.name === '➡') {
-			msg.edit(s)
-		} else {
-			msg.edit(embed)
-		}
-	})
-	.catch(collected => {
-		message.reply('you reacted with neither a thumbs up, nor a thumbs down.');
-	});
-                   }
-                   }
-                   
+    name: e
