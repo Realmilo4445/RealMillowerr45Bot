da0151b744c7f64d4c5de722ab52d8c58d047c21
Glitch (hello-express) <none>
2021-01-20 05:51:48 +0000

    🏰⛩ Checkpoint ./commands/economy/store.js:25062700/303

--- commands/economy/store.js
+++ commands/economy/store.js
@@ -1,26 +1,24 @@
 
 const Discord = require('discord.js');
-
+const nextPageEmoji = "➡"
 module.exports = {
     name: "store",
   category: "economy",
     description: "View the store",
 
     run: async (client, message, args) => {
-
+let msg = await message.send({embed});
         const embed = new Discord.MessageEmbed()
         .setTitle('Store')
         .setDescription(`**(🚗)Car** - **500** Moneys \n **(⏰)Watch** - **250** Moneys \n **(🎁)Lootbox** - **300** Moneys`)
         .setTimestamp();
-        let rct = message.react(`➡`)
-        let msg = await message.channel.send(embed)
-        
-        if(!rct) {
+        await msg.addReaction(nextPageEmoji);
+        let mesg = await message.channel.send(embed)
+
         let s = new Discord.MessageEmbed()
         .setTitle('Store')
         .setDescription(`**(💍)Ring** - **500** Moneys \n **(⏰)Watch** - **250** Moneys \n **(🎁)Lootbox** - **300** Moneys`)
         .setTimestamp();
-        msg.edit(s)
+        mesg.edit(s)
           }
       }
-}
