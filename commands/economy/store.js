743ae0f0487ca2fb9980c3e934f7be3691f118f9
Glitch (hello-express) <none>
2021-01-20 06:37:29 +0000

    🌂🙈 Checkpoint ./commands/economy/store.js:25062700/1398

--- commands/economy/store.js
+++ commands/economy/store.js
@@ -13,17 +13,17 @@ module.exports = {
         .setDescription(`**(🚗)Car** - **500** Moneys \n **(⏰)Watch** - **250** Moneys \n **(🎁)Lootbox** - **300** Moneys`)
         .setTimestamp();
       
-      let s = new Discord.MessageEmbed()
-        .setTitle('Store')
-        .setDescription(`**(💍)Ring** - **500** Moneys \n **(⏰)Watch** - **250** Moneys \n **(🎁)Lootbox** - **300** Moneys`)
-        .setTimestamp();
-        await msg.edit(s)
-        let msg = message.channel.send(embed)
+        
+        let msg = await message.channel.send(embed)
         await msg.react(nextPageEmoji)
         await msg.react(nextPreviousEmoji)
-        await msg.awaitReactions((reaction, user) => user.id == user.id && (reaction.emoji.name == nextPageEmoji||reaction.emoji.name == nextPreviousEmoji),{max: 1, time: 10000})
+        await msg.awaitReactions((reaction, user) => user.id == user.id && (reaction.emoji.name == nextPageEmoji||reaction.emoji.name == nextPreviousEmoji))
         .then(async collected => {
-          if(collected.first().emoji.name == nextPageEmoji)
-        }).catch(async () {return message.channel.send("")})
-          }
+          if(collected.first().emoji.name == nextPreviousEmoji){let s = new Discord.MessageEmbed()
+        .setTitle('Store')
+        .setDescription(`**(💍)Ring** - **500** Moneys \n **(⏰)Watch** - **250** Moneys \n **(🎁)Lootbox** - **300** Moneys`)
+        .setTimestamp(); await msg.edit(s) }
+          if(collected.first().emoji.name == nextPageEmoji){ await msg.edit(embed) }
+        })
       }
+    }
