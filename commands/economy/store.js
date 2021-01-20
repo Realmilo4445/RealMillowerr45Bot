ba7f3cb6c02b5a33737a075b2a6ef98b85fff258
Glitch (hello-express) <none>
2021-01-20 11:05:02 +0000

    🗻🐛 Checkpoint ./commands/economy/store.js:25062700/600

--- commands/economy/store.js
+++ commands/economy/store.js
@@ -9,47 +9,49 @@ module.exports = {
 
         const one = new Discord.MessageEmbed()
         .setTitle('Store')
-        .setDescription(`**(🚗)Car** - **500** Moneys \n **(⏰)Watch** - **250** Moneys`)
+        .settITLE('Car Shop')
+        .setDescription(`**(🚗)Car** - **500** Moneys \n **(🏎)Sport Car** - **550** Moneys \n **(🚙)Sport Utility Car** - **300** Moneys`)
         .setFooter(`Page 1/4`)
         .setTimestamp();
 
-        let msg = message.channel.send(one);
+        let msg = message.channel.send();
       
       const two = new Discord.MessageEmbed()
         .setTitle('Store')
-        .setDescription(`**(🚗)Car** - **500** Moneys \n **(⏰)Watch** - **250** Moneys`)
+        .setAuthor('Food Shop')
+        .setDescription(`**(🍔)Hamburger** - **5** Moneys \n **(🍕)Pizza** - **3** Moneys  \n **(🌭)Hotdog** - **4** Moneys`)
         .setFooter(`Page 2/4`)
         .setTimestamp();
       
       const three = new Discord.MessageEmbed()
         .setTitle('Store')
-        .setDescription(`**(🚗)Car** - **500** Moneys \n **(⏰)Watch** - **250** Moneys`)
-        .setFooter(`Page 2/4`)
+        .setDescription(`**(🎁)Common Lootbox** - **200** Moneys \n **(🎁)Rare Lootbox** - **250** Moneys **(🎁)Epic Lootbox** - **450** Moneys **(🎁)Legendary Lootbox** - **1,000** Moneys`)
+        .setFooter(`Page 3/4`)
         .setTimestamp();
       
       const four = new Discord.MessageEmbed()
         .setTitle('Store')
         .setDescription(`**(🚗)Car** - **500** Moneys \n **(⏰)Watch** - **250** Moneys`)
-        .setFooter(`Page 2/4`)
+        .setFooter(`Page 4/4`)
         .setTimestamp();
       
-msg.react('1️⃣').then(() => msg.react('2️⃣').then(() => msg.react('3️⃣')).then(() => msg.react('')));
+msg.react('1️⃣').then(() => message.react('2️⃣').then(() => message.react('3️⃣')).then(() => message.react('4️⃣')));
 
 const filter = (reaction, user) => {
-	return ['⏪', '◀', '▶', '⏩'].includes(reaction.emoji.name) && user.id === message.author.id;
+	return ['1️⃣', '2️⃣', '3️⃣', '4️⃣'].includes(reaction.emoji.name) && user.id === message.author.id;
 };
 
 msg.awaitReactions(filter, { max: 4, time: 60000, errors: ['time'] })
 	.then(collected => {
 		const reaction = collected.first();
 
-		if (reaction.emoji.name === '⏩') {
-			msg.edit(two)
-		} else if (reaction.emoji.name === '⏪') {
+		if (reaction.emoji.name === '1️⃣') {
 			msg.edit(one)
-    } else if (reaction.emoji.name === '⏪') {
+		} else if (reaction.emoji.name === '2️⃣') {
 			msg.edit(two)
-    } else if (reaction.emoji.name === '⏩') {
+    } else if (reaction.emoji.name === '3️⃣') {
+			msg.edit(three)
+    } else if (reaction.emoji.name === '4️⃣') {
 			msg.edit(four)
     }
 	})
