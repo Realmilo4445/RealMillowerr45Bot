f72c210d7e8eb4da70f4e0ec9db5a7d8a899d4cb
Glitch (solstice-neat-vulcanodon) <none>
2021-01-21 08:48:46 +0000

    🚦🏓 Checkpoint ./commands/economy/leaderboard.js:25062700/1698

--- commands/economy/leaderboard.js
+++ commands/economy/leaderboard.js
@@ -1,23 +1,29 @@
-const Discord = require('discord.js')
+  
 const db = require('quick.db');
+const Discord = require('discord.js');
+
 module.exports = {
     name: "leaderboard",
     category: "economy",
-    description: "Work your a** off",
+    description: "Check the sever's leaderboard",
+
     async run (client, message, args) {
-      let money = db.startsWith(`money_${message.guild.id}`, { sort: '.data'})
-      let content = ""
-      
-      for(let i = 0; i < money.length; i++) {
-        let user = message.bot.users.get(money[i].ID.split("_")[2]).username
-        
-        content += `${i+1}. ${user} ~ ${money[i].data}$\n`
-      }
-      
-      const embed = new Discord.RichEmbed()
-      .setAuthor(`${message.guild.name} - Leaderboard!`, message.guild.iconURL)
-      .setDescription(content)
-      .setColor(`RED`)
-      message.channel.send(embed)
+        let money = db.startsWith(`money_${message.guild.id}`, { sort: '.data' })
+
+        let content = "";
+
+        for (let i = 0; i < money.length; i++){
+            let user = client.users.cache.get(money[i].ID.split('_')[2]).username
+
+            content += `${i+1}. ${user} - ${money[i].data} \n`;
+
+            const embed = new Discord.MessageEmbed()
+            .setTitle(`${message.guild.name}'s Leaderboard`)
+            .setDescription(`${content}`)
+            .setColor("RED")
+            .setTimestamp()
+
+            message.channel.send(embed);
+        }
     }
 }
