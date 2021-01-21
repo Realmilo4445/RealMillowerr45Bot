e89f6613247a131c726de0b30a1b3b7b153e5c96
Glitch (solstice-neat-vulcanodon) <none>
2021-01-21 07:16:17 +0000

    🍬🎷 Checkpoint ./commands/economy/work.js:25062700/706 ./quiz.json:25062700/869

--- commands/economy/work.js
+++ commands/economy/work.js
@@ -57,22 +57,26 @@ module.exports = {
     async run (client, message, args) {
     
 const quiz = require('../../quiz.json');
+      let user = message.author
 const item = quiz[Math.floor(Math.random() * quiz.length)];
 const filter = response => {
 	return item.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());
 };
 let em = new MessageEmbed()
 em.setAuthor(item.question)
-message.channel.send(em).then(() => {
+let msg = await message.channel.send(em).then(() => {
 	message.channel.awaitMessages(filter, { max: 1, time: 30000, errors: ['time'] })
 		.then(collected => {
     let me = new MessageEmbed()
-			me.setAuthor(quiz.correct)
-      message.channel.send(me)
+			me.setAuthor(item.correct)
+      db.add(`money_${message.guild.id}_${user.id}`, item.amount)
+      me.setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic : true }))
+       message.channel.send(me)
 		})
 		.catch(collected => {
 			let mem = new MessageEmbed()
-      mem.setAuthor(quiz.fail)
+      mem.setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic : true }))
+      mem.setAuthor(item.fail)
     message.channel.send(mem)
 		});
 })
