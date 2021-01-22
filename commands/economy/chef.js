30f5faa087e2eeec65d50dea1507b48bf245403b
Glitch (solstice-neat-vulcanodon) <none>
2021-01-22 09:51:56 +0000

    👼🍗 Checkpoint ./quiz.json:25062700/191 ./commands/info/update.js:25062700/653 ./commands/economy/waiter.js:25062700/1375 ./chefprofesional.json:25062700/2560 ./commands/economy/chefprofesional.js:25062700/8

---
+++ commands/economy/chef.js
@@ -0,0 +1,43 @@
+const db = require('quick.db');
+const ms = require('parse-ms');
+const { MessageEmbed } = require('discord.js')
+const { COLOR } = require('../../config.json')
+
+
+module.exports = {
+    name: "chef",
+    category: "economy",
+    description: "Work your a** off",
+    async run (client, message, args) {
+    
+const quiz = require('../../chefprofesional.json');
+      let user = message.author
+const item = quiz[Math.floor(Math.random() * quiz.length)];
+const filter = response => {
+	return item.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());
+};
+let em = new MessageEmbed()
+.setColor(COLOR)
+em.setAuthor(item.question)
+let msg = await message.channel.send(em).then(() => {
+	message.channel.awaitMessages(filter, { max: 1, time: 30000, errors: ['time'] })
+		.then(collected => {
+    let me = new MessageEmbed()
+			me.setAuthor(item.correct)
+      .setColor(COLOR)
+      db.add(`money_${message.guild.id}_${user.id}`, item.amount)
+      me.setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic : true }))
+       message.channel.send(me)
+		})
+		.catch(collected => {
+			let mem = new MessageEmbed()
+      mem.setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic : true }))
+      mem.setAuthor(item.fail)
+      .setColor(COLOR)
+    message.channel.send(mem)
+		});
+})
+      
+      
+    }
+}
---
+++ commands/info/update.js
@@ -0,0 +1,19 @@
+const { MessageEmbed } = require("discord.js")
+const { COLOR } = require("../.././config.json")
+
+module.exports = {
+    name: "update",
+    category: "info",
+    description: "info/update",
+    run: async (client, message, args) => {
+      
+    let embed = new MessageEmbed()
+    .setColor(COLOR)
+      
+    if(!args.length) {
+      embed.setAuthor("If i didn't responding because we making new command or fixing bug || new command = 'waiter' command!")
+       message.channel.send(embed)
+    }
+    }
+  
+}
