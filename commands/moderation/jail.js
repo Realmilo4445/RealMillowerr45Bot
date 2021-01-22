2580036eb3b141a656e8ce49864d3ef10ca50f77
Glitch (solstice-neat-vulcanodon) <none>
2021-01-22 04:02:06 +0000

    🕰🍠 Checkpoint ./commands/moderation/prison.js:25062700/484 ./commands/moderation/jail.js:25062700/171

--- commands/moderation/jail.js
+++ commands/moderation/jail.js
@@ -2,33 +2,40 @@ const Discord = require('discord.js')
 const ms = require('ms')
 const All = ["Admin","Host","Moderator","Artist","Member","Level 5","Level 10","Level 25","Level 50","Youtuber","POTD Ping","Announcement Ping","Patreon [3]","Patreon [2]","Patreon [1]","Youtube Membership","Boosted","🌟VIP","???","Bot","👑Owner","!","Verified","Youtube Ping","Tweet Ping"]
 module.exports = {
-  name: "prison",
+  name: "jail",
   category: "moderation",
   description: "go to prison!",
   run: async(client, message, args) => {
     if (!message.member.hasPermission("MANAGE_ROLES")) {
       return message.channel.send(
-        "Sorry but you do not have permission to prison anyone"
+        "Sorry but you do not have permission to jail anyone"
       );
     }
-     if(!message.member.hasPermission("ADMINISTRATOR")) { return message.channel.send("You are not allowed or do not have permission to prison everyone")    }
+    let targetUser = message.mentions.members.first()
+     if(!message.member.hasPermission("ADMINISTRATOR")) { return message.channel.send("You are not allowed or do not have permission to jail everyone")    }
     const { guild } = message
     const role = guild.roles.cache.find((role) => {
       return role.name === "Inmate"
     })
-    const rolea = guild.roles.cache.find((role) => {
+    const rolea = guild.roles.cache.find((rolea) => {
       return rolea.name === All
     })
     
-    const member = message.mentions.user.first().username
+    const member = guild.member.cache.get(targetUser.id)
     member.roles.add(role)
     member.roles.remove(All)
-    let time = "10s",
-    const embed = new Discord.MessageEmbed()
-    .setAuthor(`Successfully prison **${member}**!`)
+    let embed = new Discord.MessageEmbed()
+    .setAuthor(`Successfully jailde **${targetUser}**!`)
     .setColor(`GREEN`)
     .setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic : true }))
     .setTimestamp()
-    message.channel.send(embed)
+    let msg = message.channel.send(embed)
+    let time = "15s"
+    setTimeout(function(){
+      member.roles.add("Member","Verified")
+      let m = new Discord.MessageEmbed()
+      .setAuthor(`${targetUser} has free`)
+      msg.edit(m)
+    },ms (time))
 }
 }
