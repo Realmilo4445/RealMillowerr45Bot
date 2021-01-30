7b9c63ca47a29f7defbebd912c86ecd9c401fd47
Glitch (solstice-neat-vulcanodon) <none>
2021-01-30 12:41:00 +0000

    👍👳 Checkpoint ./commands/economy/leaderboard.js:25062700/300

--- commands/economy/leaderboard.js
+++ commands/economy/leaderboard.js
@@ -9,8 +9,15 @@ module.exports = {
   run: async(client, message, args)=> {
     let say = args.join(" ")
     
-    if(say === "rank" ||say === "Rank") {
+    if(say === "economy" ||say === "Economy") {
       let money = db.startWith(`money_${message.guild.id}`, { sort: '.data'})
+      let content = ""
+      
+      for(let i = 0;i < money.length; i++) {
+        let user = client.users.get(money[i].ID.split("_")[2]).username
+        
+        content += `${i+1}. ${user} ~ $(money[i].data}`
+      }
     }
   }
   }
