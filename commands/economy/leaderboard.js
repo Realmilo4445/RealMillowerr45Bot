764efdc0a63e196dc9ce8d193830384c8abd9748
Glitch (solstice-neat-vulcanodon) <none>
2021-01-30 12:29:35 +0000

    🍶🚦 Checkpoint ./commands/economy/leaderboard.js:25062700/338

--- commands/economy/leaderboard.js
+++ commands/economy/leaderboard.js
@@ -1,9 +1,16 @@
 const db = require("quick.db")
+const discord = require("discord.js")
 
 module.exports = {
   name: "leaderboard",
   description: "leaderboard",
-  
-  
-  
+  usage: "leaderboard",
+  category: "economy",
+  run: async(client, message, args)=> {
+    let say = args.join(" ")
+    
+    if(say === "rank" ||say === "Rank") {
+      let money = db.startWith(`money_${message.guild.id}`, { sort: '.data'})
+    }
+  }
   }
