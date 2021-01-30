2dd899ce8058c9b723b36e3416cd72473df5a665
Glitch (solstice-neat-vulcanodon) <none>
2021-01-30 12:52:57 +0000

    🎃🎤 Checkpoint ./commands/economy/leaderboard.js:25062700/304

--- commands/economy/leaderboard.js
+++ commands/economy/leaderboard.js
@@ -10,14 +10,22 @@ module.exports = {
     let say = args.join(" ")
     
     if(say === "economy" ||say === "Economy") {
-      let money = db.startWith(`money_${message.guild.id}`, { sort: '.data'})
+      let money = db.startsWith(`money_${message.guild.id}`, { sort: '.data'})
       let content = ""
       
       for(let i = 0;i < money.length; i++) {
         let user = client.users.get(money[i].ID.split("_")[2]).username
         
-        content += `${i+1}. ${user} ~ $(money[i].data}`
+        content += `${i+1}. ${user} ~ $(money[i].data}$\n`
+        
+        
       }
+      let embed = new discord.RichEmbed()
+      .setAuthor(`${message.guild.name} - Economy Leaderboard!`, message.guild.iconURL)
+      .setDescription(content)
+      .setTimestamp()
+      
+      message.channel.send(embed)
     }
   }
   }
