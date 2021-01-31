fb76bafcfd55d88bb6543a9b9e903a4f574b2d34
Glitch (solstice-neat-vulcanodon) <none>
2021-01-31 03:31:14 +0000

    🌛👋 Checkpoint ./commands/economy/leaderboard.js:25062700/517

--- commands/economy/leaderboard.js
+++ commands/economy/leaderboard.js
@@ -18,9 +18,17 @@ module.exports = {
     let lastpage = Math.ceil(Object.Keys(data).length / limit)
     let page = parseInt(args[0]);
     if(!page) page = 1;
-    if(page > lastpage) return message.channel.send(`Sorry, there is no **page ${page}**.`)
+    if(page > lastpage) return message.channel.send(`Sorry, there is no **page ${page}**.`);
       
+    let frompages = limit * (page - 1);
+    let pageslimit = 15 * page;
       
-      }
+    let list = Object.entries(data).sort((a, b, c) => b[1].money - a[1].worked - c[1].robbed).slice(frompages, pageslimit);
+    let arr = []
+    
+    for(var i in list) {
+      arr.push(`**${i * 1 +1 + frompages}.** ${message.guild.members.cache.get(list[i][0]) ? message.guild.members.cache.get(list[i][0]).user.tag : "UnknownUser"} - Money: **${list[i][1].money}**`)
+    }
+   }
   }
 }
