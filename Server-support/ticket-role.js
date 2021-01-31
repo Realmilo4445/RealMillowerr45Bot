7b3c7ee74db38705fea5787c7f778cad24cb4016
Glitch (solstice-neat-vulcanodon) <none>
2021-01-31 13:16:09 +0000

    ✌️☂️ Checkpoint ./Server-support/ticket-role.js:25062700/371

--- Server-support/ticket-role.js
+++ Server-support/ticket-role.js
@@ -1,2 +1,12 @@
 const { DiscordTicket } = require('discord_ticket_maker')
-const {
+const ticket = new DiscordTicket()
+const { MessageEmbed } = require('discord.js')
+
+module.exports = {
+  name: "ticket-role",
+  caregory: "support_server",
+  description: "Set support ticket role",
+  run: async(client, message, args) => {
+    
+  }
+}
