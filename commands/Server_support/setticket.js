0d574388fe898f7b795aaf9df8310ab64ab1675c
Glitch (solstice-neat-vulcanodon) <none>
2021-02-01 02:02:03 +0000

    🚕🐠 Checkpoint ./commands/Server_support/setticket.js:25062700/273

--- commands/Server_support/setticket.js
+++ commands/Server_support/setticket.js
@@ -1,17 +1,20 @@
 const Discord = require('discord.js')
-const db = require("")
+const db = require("quick.db")
 
 module.exports = {
-    name: 'endticket',
-    description: "ends the ticket",
-    execute(client, message, args){
+    name: 'setticket',
+    aliases: ["s-ticket"],
+    usage: "setticket <question>",
+    category: "support_server",
+    description: "set the server support ticket",
+    run: async(client, message, args) => {
         if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("(:x:)Only a moderator can set a ticket!")
 
         if(message.member.hasPermission("ADMINISTRATOR")) message.channnel.delete()
       
       let say = args.join(" ")
       if(!say)return message.channel.send("(:x:)Please type valid question for ticket!")
-      
+      let channel = message.guild.channels
     }
 
 }
