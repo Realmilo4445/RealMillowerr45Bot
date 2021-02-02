ad6787fc04b967c90226cc30ce5c93584fa7a408
Glitch (solstice-neat-vulcanodon) <none>
2021-02-02 15:35:22 +0000

    🌪🚦 Checkpoint ./commands/info/channel.js:25062700/750

--- commands/info/channel.js
+++ commands/info/channel.js
@@ -1,3 +1,19 @@
 const Discord = require('discord.js')
 
-module.export
+module.exports = {
+  name: 'channel',
+  usage: 'channel <channelmention',
+  description: 'channel mention',
+  category: 'info',
+  run: async(client, message, args) => {
+    const channel = message.mentions.channels.first() || message.guild.channels.cache.get(args[0])
+    if(!channel)return message.channel.send("Please mention channel for channel id!")
+    
+    let embed = new Discord.MessageEmbed()
+    .setAuthor("Channel id:" + channel.id)
+    .setColor(`RANDOM`)
+    .setFooter(message.author.tag, message.author.displayAvatarURL({dynamic: true}))
+    .setTimestamp()
+    message.channel.send(embed)
+  }
+}
