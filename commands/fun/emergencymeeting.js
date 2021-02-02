6bf43be5d02f8bf20fcd0bff9777b1dc62396c50
Glitch (solstice-neat-vulcanodon) <none>
2021-02-02 02:16:16 +0000

    🕋🚃 Checkpoint ./commands/fun/emergencymeeting.js:25062700/74

--- commands/fun/emergencymeeting.js
+++ commands/fun/emergencymeeting.js
@@ -6,12 +6,13 @@ module.exports = {
   description: 'someone is impostor!',
   category: 'fun',
   run: async(client, message, args) => {
-    const Canvas = canvas.createCanvas(700, 250)
-    const ctx = canvas.getContext('2d')
+    const Canvas = canvas.createCanvas(612, 500)
+    const ctx = Canvas.getContext('2d')
     const background = await canvas.loadImage('https://cdn.discordapp.com/attachments/777065805401686027/805976093513351198/images.jpeg')
     ctx.drawImage(background, 0,0, Canvas.width, Canvas.height)
-    ctx.fillText(args, 50, 80)
-    const attachment = new Discord.MessageAttachment(Canvas.tobuffer(), 'emergencymeeting.png')
+    ctx.fillText(args, 50, 100)
+    ctx.font = '1000px Impact'
+    const attachment = new Discord.MessageAttachment(Canvas.toBuffer(), 'emergencymeeting.png')
     message.channel.send(attachment)
   }
 }
