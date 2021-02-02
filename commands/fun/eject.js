74029db9c24762805ee7159d0e89cabab98cc163
Glitch (solstice-neat-vulcanodon) <none>
2021-02-02 02:50:16 +0000

    📷📻 Checkpoint ./commands/fun/meme.js:25062700/229 ./commands/fun/emergencymeeting.js:25062700/1337 ./commands/fun/eject.js:25062700/1608

---
+++ commands/fun/eject.js
@@ -0,0 +1,28 @@
+const canvas = require('canvas')
+const Discord = require('discord.js')
+
+module.exports = {
+  name: 'emergencymeeting',
+  description: 'someone is impostor!',
+  category: 'fun',
+  run: async(client, message, args) => {
+    const Canvas = canvas.createCanvas(612, 500)
+    const ctx = Canvas.getContext('3d')
+    here source code: const canvas = require('canvas')
+const Discord = require('discord.js')
+
+module.exports = {
+  name: 'emergencymeeting',
+  description: 'someone is impostor!',
+  category: 'fun',
+  run: async(client, message, args) => {
+    const Canvas = canvas.createCanvas(612, 500)
+    const ctx = Canvas.getContext('2d')
+    const background = await canvas.loadImage('https://cdn.discordapp.com/attachments/777065805401686027/805976093513351198/images.jpeg')
+    ctx.drawImage(background, 0,0, Canvas.width, Canvas.height)
+    ctx.fillText(args, 50, 100)
+    ctx.font = '30px Arial'
+    const attachment = new Discord.MessageAttachment(Canvas.toBuffer(), 'emergencymeeting.png')
+    message.channel.send(attachment)
+  }
+}
--- commands/fun/meme.js
+++ commands/fun/meme.js
@@ -10,7 +10,7 @@ module.exports = {
   
     let data = await random.getMeme()
     message.channel.send(data)
-  const lol = "40:11"
+  const lol = "https://www.google.com/url?sa=i&url=https%3A%2F%2Ftenor.com%2Fsearch%2Fejected-gifs&psig=AOvVaw2fcgYo1jAVv3L_NJl6vXKM&ust=1612320237315000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCPCwx_6Wyu4CFQAAAAAdAAAAABADCFQAAAAAdAAAAABAD"
   
 }
 }
