901f589e5107e2484f19d48813ab78a0404f9c12
Glitch (solstice-neat-vulcanodon) <none>
2021-02-02 06:00:43 +0000

    🐞💅 Checkpoint ./commands/economy/sell.js:25062700/2065

--- commands/economy/sell.js
+++ commands/economy/sell.js
@@ -6,6 +6,27 @@ module.exports = {
   usage: 'sell <item> <price>',
   description: 'sell your item!',
   run: async(client, message, args) => {
+    const price = args.join(" ")
+    let say = args.join(" ")
+    let items = await db.fetch(message.author.id)
+    if(items === null) items = "Nothing"
+    if(!items === null) return message.channel.send("You dont have item to sell!")
+        if (!price[0]) return message.reply("Please enter the amount of item price to sell!");
+ 
+        if(isNaN(price[0])) return message.reply("Please type a real number!");
+ 
+        if(price[0] > 100) return message.reply("You can't sell more than 100 moneys item price!");
+        
+        if(price[0] < 1) return message.reply("You have to sell at least one money item price!")
     
+    if(say === {items} + {price}) {
+      let embed = new Discord.MessageEmbed()
+      .setAuthor(`Succesfully sell your item`)
+      .setDescription(items + price)
+      .set
+      db.subract(items)
+      let msg = await message.channel.send(embed)
+      
+    }
   }
 }
