43533bda87f0f809f889fbe78ff93e23d9f9a05b
Glitch (solstice-neat-vulcanodon) <none>
2021-02-02 14:13:13 +0000

    ⛳️🚍 Checkpoint ./commands/info/say.js:25062700/526

--- commands/info/say.js
+++ commands/info/say.js
@@ -2,5 +2,16 @@ const Discord = require('discord.js')
 
 module.exports = {
   name: 'say',
-  description: '
+  category: 'info',
+  description: 'say if you want',
+  usage: 'say <args>',
+  run: async(client, message, args) => {
+    let saying = args.join (" ")
+    let embed = new Discord.MessageEmbed()
+    .setAuthor(saying)
+    .setColor(`RANDOM`)
+    .setFooter(message.author.tag, message.author.displayAvatarURL({dynamic: true}))
+    .setTimeStamp()
+    message.channel.send(embed)
+  }
 }
