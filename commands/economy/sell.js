eb874d6f41f351b71e132e853c9ce8af47a40be3
Glitch (solstice-neat-vulcanodon) <none>
2021-02-02 05:48:12 +0000

    🌽🔋 Checkpoint ./commands/economy/sell.js:25062700/288 ./commands/moderation/clear.js:25062700/1672

---
+++ commands/economy/sell.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js')
+const db = require('quick.db')
+module.exports = {
+  name: 'sell',
+  category: 'economy',
+  usage: 'sell <item> <price>',
+  description: 'sell your item!',
+  run: async(client, message, args) => {
+    
+  }
+}
