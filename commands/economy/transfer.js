36ba6a94159c6d5fe0165a90c2087670fb37315f
Glitch (solstice-neat-vulcanodon) <none>
2021-02-03 01:47:48 +0000

    🚋🍔 Checkpoint ./commands/economy/transfer.js:25062700/2

--- commands/economy/transfer.js
+++ commands/economy/transfer.js
@@ -18,11 +18,11 @@ let  e = db.fetch(`money_${message.guild.id}_${message.author.id}`)
 let user = message.mentions.users.first().username
 if(!user) return message.channel.send("Please mention people for transfer!")
 
-        if(message.content.includes('-')) return message.reply("(:x:)Please dont transfer minus money!")
+        if(!message.content.includes('-')) return message.reply("(:x:)Please dont transfer minus money!")
 
         
 
-        if(e < 1) return message.reply("(:x:)You dont have money to transfer!");
+        if(!e < 1) return message.reply("(:x:)You dont have money to transfer!");
 
 let embed = new Discord.MessageEmbed()
 embed.setDescription(`Successfully transfered ${es} money(s) to ${user}!`)
