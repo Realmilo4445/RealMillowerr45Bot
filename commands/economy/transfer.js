2e01d005ee26bb45eacb47f0c53647124495b87c
Glitch (solstice-neat-vulcanodon) <none>
2021-02-03 01:13:41 +0000

    👐🛋 Checkpoint ./commands/economy/transfer.js:25062700/1015

--- commands/economy/transfer.js
+++ commands/economy/transfer.js
@@ -13,35 +13,21 @@ aliases: ["trans"],
     description: "transfered to user",
 
    run: async(client, message, args) => {
-let  e = args.join(" ")
-let user = message.author || message.mentions.users.first().username
-        if (!e[0]) return message.reply("(:x:)Please enter the amount of moneys to transfer!");
+let  e = db.fetch(`money_${message.author.id}`)
+let user = message.mentions.users.first().username
+if(!user) return message.channel.send("Please mention people for transfer!")
+
+        if(message.content.includes('-')) return message.reply("(:x:)Please !");
 
  
 
-        if(isNaN(e[0])) return message.reply("(:x:)Please type a real number!");
-
- 
-
-        if(e[0] > 100) return message.reply("(:x:)You can't transfer more than 100 moneys!");
+        if(e > 100) return message.reply("(:x:)You can't transfer more than 100 moneys!");
 
         
 
-        if(e[0] < 1) return message.reply("(:x:)You have to transfer at least one money!");
+        if(e < 1) return message.reply("(:x:)You dont have money to transfer!");
 
- let timeout = 99990;
-      
-
-        let daily = await db.fetch(`daily_${message.guild.id}_${user.id}`);
-        if(daily !== null && timeout - (Date.now() - daily) > 0){
-            let time = ms(timeout - (Date.now() - daily));
-          Embed.setAuthor(`(❌)You've already transfered. Come back in ${time.days}d, ${time.hours}h, ${time.minutes}m, and ${time.seconds}s`)
-            return message.channel.send(Embed)
-        } else {
 let embed = new Discord.MessageEmbed()
-db.set(`daily_${message.guild.id}_${user.id}`, Date.now())
-db.add(`money_${message.guild.id}_${user.id}`, e)
-db.subtract(`money_${message.guild.id}_${message.author.id}`, e)
 embed.setDescription(`Successfully transfered ${args} money(s) to ${user}!`)
 
 message.channel.send(embed)
@@ -49,5 +35,3 @@ message.channel.send(embed)
  
 }
  }
-
-}   
