243b00c041ee21baba68eb494c3975101a2d6646
Glitch (solstice-neat-vulcanodon) <none>
2021-02-03 15:48:22 +0000

    👑🎪 Checkpoint ./config.json:25062700/118 ./commands/moderation/kick.js:25062700/94

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -9,28 +9,24 @@ module.exports = {
     
 
 if(!message.member.hasPermission("KICK_MEMBERS")) {
-  message.delete()
       return message.channel.send(`(❌)**${message.author.username}**, You do not have enough permission to use this command`)
     }
 
 if(!message.guild.me.hasPermission("KICK_MEMBERS")) {
-  message.delete()
       return message.channel.send(`(❌)**${message.author.username}**, I do not have enough permission to use this command`)
     }
     
-     let target = message.mentions.members.first();
+     let target = message.mentions.users.first();
     
     if(!target) {
-      message.delete()
       return message.channel.send(`(❌)**${message.author.username}**, Please mention the person who you want to kick`)
     }
 
 if(target.id === message.author.id) {
-     return message.channel.send(`**${message.author.username}**, You can not kick yourself`)
+     return message.channel.send(`**${message.author.username}**, You catn not kick yourself`)
     }
 
  if(!args[1]) {
-   message.delete()
     return message.channel.send(`(❌)**${message.author.username}**, Please Give Reason to kick`)
   }
 
