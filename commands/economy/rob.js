e5070d09695495b0ff94527b11ce9f24962ef670
Glitch (solstice-neat-vulcanodon) <none>
2021-02-04 13:06:50 +0000

    📯🔨 Checkpoint ./commands/economy/rob.js:25062700/295

--- commands/economy/rob.js
+++ commands/economy/rob.js
@@ -21,35 +21,31 @@ if (author !== null && timeout - (Date.now() - author) > 0) {
     let time = ms(timeout - (Date.now() - author));
 
     let timeEmbed = new Discord.RichEmbed()
-    .setColor("#FFFFFF")
-    .setDescription(`<:Cross:618736602901905418> You have already robbed someone\n\nTry again in ${time.minutes}m ${time.seconds}s `);
+    .setDescription(`(❌) You have already robbed someone\n\nTry again in ${time.minutes}m ${time.seconds}s `);
     message.channel.send(timeEmbed)
   } else {
 
 let moneyEmbed = new Discord.RichEmbed()
-  .setColor("#FFFFFF"
-  .setDescription(`<:Cross:618736602901905418> You need atleast 200 coins in your wallet to rob someone`);
+  .setDescription(`(❌)You need at least 200 coins in your wallet to rob someone`);
 
 if (author2 < 200) {
     return message.channel.send(moneyEmbed)
 
 }
 let moneyEmbed2 = new Discord.RichEmbed()
-  .setColor("#FFFFFF")
-  .setDescription(`<:Cross:618736602901905418> ${user.user.username} does not have anything you can rob`);
+  .setDescription(`(❌)${user.user.username} does not have anything you can rob`);
 if (targetuser < 0) {
     return message.channel.send(moneyEmbed2)
 }
 
-
+let random = Math.floor(Math.random()*15) + 1;
 
 let vip = await db.fetch(`bronze_${user.id}`)
 if(vip === true) random = Math.floor(Math.random() * 200) + 1;
 if (vip === null) random = Math.floor(Math.random() * 100) + 1;
 
 let embed = new Discord.RichEmbed()
-.setDescription(`<:Check:618736570337591296> You robbed ${user} and got away with ${random} coins`)
-.setColor("#FFFFFF")
+.setDescription(`(✅) You robbed ${user} and got away with ${random} coins`)
 message.channel.send(embed)
 
 db.subtract(`money_${message.guild.id}_${user.id}`, random)
@@ -60,4 +56,3 @@ db.set(`rob_${message.guild.id}_${user.id}`, Date.now())
 }
 
     }
-}
