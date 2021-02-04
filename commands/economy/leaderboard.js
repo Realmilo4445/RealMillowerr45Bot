786728a087e54c6672ca0e64f03f651bd7e10975
Glitch (solstice-neat-vulcanodon) <none>
2021-02-04 10:04:58 +0000

    👨‍❤️‍👨🐟 Checkpoint ./commands/economy/leaderboard.js:25062700/79

--- commands/economy/leaderboard.js
+++ commands/economy/leaderboard.js
@@ -10,11 +10,11 @@ module.exports = {
     let lb = db.all().filter(a => a.ID.startsWith('money_')).sort((a, b) => b.data - a.data)
     
     let i = 0
-    let place = 0
+    let place = 1
     let txt = ""
     
     for(i in lb) {
-      txt += `${place++}. <@${lb[i].ID.split('_')[1]}> - ${lb[i].data}`
+      txt += `${place++}. <@${lb[i].NAME.split('_')[1]}> - ${lb[i].data}$\n`
     }
     
     let embed = new Discord.MessageEmbed()
