672f825f4cf31c4ad9d2d7116678eff0e8f7ed3b
Glitch (solstice-neat-vulcanodon) <none>
2021-02-05 07:02:33 +0000

    💕🍵 Checkpoint ./commands/info/avatar.js:25062700/750

--- commands/info/avatar.js
+++ commands/info/avatar.js
@@ -5,7 +5,16 @@ module.exports = {
   description: 'view user avatar',
   usage: 'avatar <@mention>',
   aliases: ['av'],
-  run: async(client, message, args) = {
-    let user = message.mentions.users.first()
+  category: 'info',
+  run: async(client, message, args) => {
+    let user  = message.mentions.users.first() || message.author || message.guild.members.cache.get(args[0])
+    if(!user) {
+    return message.channel.send(`(❌)Please mention the person to show the person avatar!`)
+  }
+    
+    let Embed = new Discord.MessageEmbed()
+    .setAuthor(user.tag)
+    .setImage(user.displayAvatarURL({dynamic: true}))
+    message.channel.send(Embed)
   }
 }
