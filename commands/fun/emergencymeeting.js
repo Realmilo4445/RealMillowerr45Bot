83a67b24c6d9e43422954613730be508c169702f
Glitch (solstice-neat-vulcanodon) <none>
2021-02-06 09:11:10 +0000

    🎖👄 Checkpoint ./commands/fun/emergencymeeting.js:25062700/10

--- commands/fun/emergencymeeting.js
+++ commands/fun/emergencymeeting.js
@@ -10,7 +10,7 @@ module.exports = {
     const ctx = Canvas.getContext('2d')
     const background = await canvas.loadImage('https://cdn.discordapp.com/attachments/777065805401686027/805976093513351198/images.jpeg')
     ctx.drawImage(background, 0,0, Canvas.width, Canvas.height)
-    ctx.fillText(args, 50, 100)
+    ctx.fillText(args.join(" "), 50, 100)
     ctx.font = '30px Arial'
     const attachment = new Discord.MessageAttachment(Canvas.toBuffer(), 'emergencymeeting.png')
     message.channel.send(attachment)
